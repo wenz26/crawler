284de2dafa2890291776e74339dda8939a6f85a8
wenz26 <948809390@qq.com>
2019-11-17 22:33:29 +0800

    Initial commit

---
+++ crawler-first/src/main/java/com/cwz/springboot/crawlerfirst/CrawlerFirstApplication.java
@@ -0,0 +1,13 @@
+package com.cwz.springboot.crawlerfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerFirstApplication.class, args);
+    }
+
+}
---
+++ crawler-first/src/test/java/com/cwz/springboot/crawlerfirst/CrawlerFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwz.springboot.crawlerfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
